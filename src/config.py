b00195b30846f62d571b802568d2093368751bc7
Wyatt Shapiro <wyattshapiro@gmail.com>
2018-05-22 19:20:05 -0400

    refactor code to make the coin name an argument instead of living in config

--- src/config.py
+++ src/config.py
@@ -4,11 +4,11 @@ import os
 # Set up
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
+HEADLESS = True
 
 # Inputs
-COIN = "ripple"  # could also be "ripple", "bitcoin", etc
-START_DATE = "20170428"
+START_DATE = "20130428"
 END_DATE = datetime.datetime.today().strftime('%Y%m%d')
 
 # Outputs
-OUTPUT_PATH = BASE_DIR + "/results/" + COIN + "_" + START_DATE + "_" + END_DATE + ".csv"
+OUTPUT_PATH = BASE_DIR + "/results/" + '[coin_name]' + "_" + START_DATE + "_" + END_DATE + ".csv"
